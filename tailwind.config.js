8a80af88db86522643152a635f069ac5dfcfa9f5
ribamarf01 <ribamar.f01@gmail.com>
2022-04-08 04:40:43 -0300

    tsconfig build opt update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,10 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: {
+    content: [],
+    options: {
+        safelist: [/(from|via|to|border|bg|text)-(.*)-(\\d{1}0{1,2})/]
+    }
+  }
 }
